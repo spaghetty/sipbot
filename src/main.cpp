0f15a629a92f8f33f169798916bfceabcf0dc7fd
Domenico Chierico <spaghetty@gmail.com>
2010-02-10 00:51:03 +0100

    start implementing ua interface

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,18 @@
 
 int main(int argc, char* argv[])
 {
-  Ua app;
-  clientEvent e1((event_type)25), e2((event_type)18);
+  Ua app("localhost",5060);
+  app.set_realm("spaghetty.net");
+  app.set_proxy("192.168.1.109");
+  printf("this is just setted proxy %s\n", app.get_proxy());
+  //app.addLine("500","500");
+  
+  /*clientEvent e1((event_type)25), e2((event_type)18);
   sipServer *s = new sipServer("prova", "");
   std::string a = s->get_uri(1);
   Line *l = new Line(&app);
   l->set_info("500","500","192.168.1.109","prova.com");
   delete s;
   printf("uri %s \n",a.c_str());
-  printf("uri %s \n",l->get_uri(true).c_str());
+  printf("uri %s \n",l->get_uri(true).c_str()); */
 }
